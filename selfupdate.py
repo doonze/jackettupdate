01860a6e99430b8e40ba58480c6c6bf2618a9212
Justin Hopper <doonze@gmail.com>
2020-05-25 16:56:41 -0500

    Modified to hanle the selfupate process in the new flow

--- selfupdate.py
+++ selfupdate.py
@@ -2,6 +2,7 @@
 # This Python file uses the following encoding: utf-8
 # This program is used can called by the main program to update the app itself if needed
 # JackettUpdate selfupdate file
+
 app = "JackettUpdate(self): "
 import sys
 import os
@@ -15,8 +16,9 @@ import subprocess
 from timeit import default_timer as timer
 from configparser import ConfigParser
 import configparser
-from customfunctions import timestamp
-import customfunctions
+from customfunctions import time_stamp, get_file, display_time, remove_file
+from configread import appversion
+print(" Self update started")
 
 selfupdatestart = timer()
 # Sets up the config system
@@ -24,20 +26,6 @@ config = configparser.ConfigParser()
 
 # I don't use beta releases, but this is here just in case I do in the future
 installbeta = False
-
-# Now we're going to open the config file reader
-config.read('config.ini')
-
-# Now we're going to open the config file reader
-config.read('config.ini')
-
-# And we're going to get the current installed version from config
-try:
-    appversion = config['JackettUpdate']['version']
-except Exception as e:
-    print(timestamp(app) + "We couldn't pull the current version from the config file!")
-    print(timestamp(app) + "Here's the error we got -- " + str(e))
-    sys.exit()
 
 # We're going to force the working path to be where the script lives
 os.chdir(sys.path[0])
@@ -64,8 +52,8 @@ try:
                 versiontype = "Stable"
                 break
 except Exception as e:
-    print(timestamp(app) + "We didn't get an expected response from the github api, script is exiting!")
-    print(timestamp(app) + "Here's the error we got -- " + str(e))
+    print(time_stamp(app) + "We didn't get an expected response from the github api, script is exiting!")
+    print(time_stamp(app) + "Here's the error we got -- " + str(e))
     print(e)
     sys.exit()
     
@@ -74,26 +62,23 @@ downloadurl = "https://github.com/doonze/JackettUpdate/archive/" + onlineversion
 zfile = onlineversion + ".zip"
 
 # Ok, we've got all the info we need. Now we'll test if we even need to update or not.
-
 onlinefileversion = (onlineversion + "-" + versiontype)
 
 if str(onlinefileversion) in str(appversion):    
     selfupdateend = timer()
-    totalupdatetime = customfunctions.display_time(selfupdateend - selfupdatestart)
-    # If the latest online version matches the last installed version then we let you know and exit
-    print("{}App is up to date! Current and Online versions are at {}. Check took {}.".format(timestamp(app), onlinefileversion, totalupdatetime))
-    print("")
-    quit()
+    totalupdatetime = display_time(selfupdateend - selfupdatestart)
+    # If the latest online version matches the last installed version then we let you know and move on
+    print("{}App is up to date! Current and Online versions are at {}. Check took {}.".format(time_stamp(app), onlinefileversion, totalupdatetime))
 else:
 	# If the online version DOESN'T match the last installed version we let you know what the versions are and start updating
     print('')
-    print(timestamp(app) + "Most recent app online version is " + onlinefileversion + " and current installed version is " + appversion + ". We're updating JackettUpdate app.")
+    print(time_stamp(app) + "Most recent app online version is " + onlinefileversion + " and current installed version is " + appversion + ". We're updating JackettUpdate app.")
     print('')
-    print("\n" + timestamp(app) + "Starting self app update, installing to " + str(sys.path[0]) + "......")
+    print("\n" + time_stamp(app) + "Starting self app update, installing to " + str(sys.path[0]) + "......")
     print('')
 
    	# Here we download the zip to install          
-    print(customfunctions.getfile(downloadurl))
+    print(get_file(downloadurl, app=app))
     
 	# Next we unzip and install it to the directory where the app was ran from
     with zipfile.ZipFile(zfile) as zip:
@@ -105,7 +90,7 @@ else:
             zip.extract(zip_info, '')
 
 	# And to keep things nice and clean, we remove the downloaded file once unzipped
-    subprocess.call("rm -f " + zfile,shell=True)
+    print(remove_file(zfile, app=app))
 
     # now we'll set the app as executable
     st = os.stat("jackettupdate.py")
@@ -113,18 +98,18 @@ else:
 
 	# Lastly we write the newly installed version into the config file
     try:
+        # Now we're going to open the config file reader        
         config['JackettUpdate']['version'] = onlinefileversion
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
     except Exception as e:
-        print(timestamp(app) + "We couldn't write the installed version to the config file!")
-        print(timestamp(app) + "Here's the error we got -- " + str(e))
+        print(time_stamp(app) + "We couldn't write the installed version to the config file!")
+        print(time_stamp(app) + "Here's the error we got -- " + str(e))
         quit()
     
     selfupdateend = timer()
-    totalupdatetime = customfunctions.display_time(selfupdateend - selfupdatestart)
+    totalupdatetime = display_time(selfupdateend - selfupdatestart)
 
     print('')
-    print("{} Updating to app version {} took {}. Script exiting!".format(timestamp(app), onlinefileversion, totalupdatetime))
-    print('')    
-    print("\n")
+    print("{} Updating to app version {} took {}.".format(time_stamp(app), onlinefileversion, totalupdatetime))
+
